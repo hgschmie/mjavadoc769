be5c4a6ef6535c668c9873854c7f52c56a9f0bc0
Henning P. Schmiedehausen <henning@schmiedehausen.org>
2023-08-20 21:27:48 -0700

    unit test

---
+++ src/test/java/mavenbugs/mjavadoc770/RightTest.java
@@ -0,0 +1,22 @@
+package mavenbugs.mjavadoc770;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+import org.junit.jupiter.api.Test;
+
+public class RightTest {
+
+  @Test
+  public void testRight() throws Exception {
+    var method = Foo.class.getMethod("foo");
+    var annotation = method.getAnnotation(Right.class);
+    assertNotNull(annotation);
+
+  }
+
+  public static class Foo {
+    @Right
+    public void foo() {
+    }
+  }
+}
